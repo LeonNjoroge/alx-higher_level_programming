0c60a31e1208dd55e209e0002e987b981ebed183
Leon <leonmuraguri@gmail.com>
2023-11-27 03:42:57 -0800

    Task Complete

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+word = "Holberton"
+print(f"First 3 letters: {word_first_3[:3]}\n")
+
+print(f"Last 2 letters: {word_last_2[-2:]}\n")
+
+print(f"Middle word: {middle_word[3:-3]}\n")
