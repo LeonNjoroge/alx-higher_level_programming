f935242e38f73833605e54d6be8e84c18076f752
LeonNjoroge <leonmuraguri@gmail.com>
2023-11-27 18:45:59 +0300

    Task Complete

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Holberton School"
-print(f"{str}{str}{str}\n")
-print(f"{str[8]}\n")
+print("{}".format(str) * 3)
+print(f"{str[:9]}")
--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-print(f"First 3 letters: {word_first_3[:3]}\n")
-
-print(f"Last 2 letters: {word_last_2[-2:]}\n")
-
-print(f"Middle word: {middle_word[3:-3]}\n")
+word_first_3 = word[:3]
+word_last_2 = word[-2:]
+middle_word = word[1:-1]
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
