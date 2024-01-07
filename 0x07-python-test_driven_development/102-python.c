11869ef393b8ce069ac69f0630ee84fecbb44be4
LeonNjoroge <leonmuraguri@gmail.com>
2024-01-07 22:39:54 +0300

    Task Done

---
+++ 0x07-python-test_driven_development/102-python.c
@@ -0,0 +1,31 @@
+#include <object.h>
+#include <unicodeobject.h>
+#include <Python.h>
+
+
+void print_python_string(PyObject *p)
+{
+    const char *kind = NULL;
+
+    Py_ssize_t length = 0;
+
+    wchar_t *stri = NULL;
+
+    printf("[.] string object info\n");
+    if (!PyUnicode_Check(p))
+    {
+        printf("  [ERROR] Invalid String Object\n");
+        return;
+    }
+
+    if (PyUnicode_IS_COMPACT_ASCII(p))
+        kind = "compact ascii";
+    else
+        kind = "compact unicode object";
+
+    stri = PyUnicode_AsWideCharString(p, &length);
+
+    printf("  type: %s\n", kind);
+    printf("  length: %ld\n", length);
+    printf("  value: %ls\n", stri);
+}
